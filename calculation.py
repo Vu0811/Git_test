2211a3e04e670ed96587afdf2a2aed94cc2b603f
“Vu0811” <thevuu2001@gmail.com>
2022-10-12 15:33:29 +0700

    feat(calculation) add cir area

--- calculation.py
+++ calculation.py
@@ -3,6 +3,8 @@ import math
 def cal_rectangle_perimeter(a, b):
     return 2*(a + b)
 
+def cal_circle_area(r):
+    return 2 * math.pi * pow(r, 2)
 
 if __name__ == '__main__':
     f = int(input("choose function: \n\
@@ -10,14 +12,15 @@ if __name__ == '__main__':
         1. cal_circle_area \n\
         2. cal_rectangle_area \nPlease enter an integer:"))
 
-    mult = int(input("Add multiplier:"))
-
     if f == 0:
         a = int(input("Input value a: "))
         b = int(input("Input value b: "))
         result = cal_rectangle_perimeter(a, b)
 
+    elif f == 1:
+        r = int(input("Input value r: "))
+        result = cal_circle_area(r)
 
     else:
         result = "Wrong input"
-    print(f"\nResult: {result * mult}")
+    print(f"\nResult: {result}")
